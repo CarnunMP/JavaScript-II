adb6518226e0407a0357c6a6b058f3b5062129c5
CarnunMP <carnun@hotmail.co.uk>
2019-08-20 12:18:08 +0100

    callbacks done; onto callback tests

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,26 +38,39 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // "this Pencil is worth a million dollars!"
 */
 
+console.log("Callbacks: ");
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let isInList = false;
+  list.forEach(i => {
+    if (!isInList && (item === i)) {
+      isInList = true;
+    }
+  });
+
+  cb(isInList);
 }
 
 /* STRETCH PROBLEM */
@@ -67,3 +80,5 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+console.log("———")
